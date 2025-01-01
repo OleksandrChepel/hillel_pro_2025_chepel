2d44bb8fc26f2d30eac597f4b6ff5eb333792f7f
Shurik <oleksandr.chepel99@gmail.com>
2025-01-01 22:29:25 +0200

    Fourth commit

---
+++ Hillel_2025/src/main/java/org/example/lesson4/Customer.java
@@ -0,0 +1,19 @@
+package org.example.lesson4;
+
+public  class Customer {
+    private final String name;
+    public final String phone;
+
+    public Customer(String name, String phone) {
+        this.name = name;
+        this.phone = phone;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+}
